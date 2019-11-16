deb659dd8a10feb36c12ab578529dc0d1a47817d
yujin610 <leesemi1215@naver.com>
2019-11-17 07:25:54 +0900

    제본예약 디비완성

--- routes/booklookup.js
+++ routes/booklookup.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Book=require("../models/Books");
+var Rsv=require("../models/Reserve");
 var errorCatcher = require('../lib/async-error'); 
 var { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
@@ -27,7 +28,9 @@ router.post('/',async function(req,res){
     professor : req.body.professor,
     price : req.body.price,
     stock : req.body.stock,
-    num_rsv : 0
+    uid : null,
+    num_rsv : 0,
+    total_rsv : 0
   });
 
   await new_book.save();
@@ -39,7 +42,6 @@ router.post('/',async function(req,res){
 });
 
 
-
 //제본예약 창
 router.get('/reserve:id',isLoggedIn, function(req,res){
   Book.findById(req.params.id, function(err, books){
@@ -47,32 +49,123 @@ router.get('/reserve:id',isLoggedIn, function(req,res){
   });
 });
 
-router.post('/reserve:id', async function(req,res){
-  console.log('hi')
-  Book.findById(req.params.id, function(err, books){
-    if(books.num_rsv+Number(req.body.count)>books.stock){
-      req.flash('fail', '잔여량보다 더 많은 양을 예약했어용');
-      return res.render('booklookup/reserve', {books:books});
-    }
-    books.num_rsv+=Number(req.body.count);
-    console.log(books.num_rsv);
+
+
+// 여기에요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+// router.post('/reserve:id', async function(req,res){
+//   console.log('hi');
+
+//   Book.findById(req.params.id, function(err, books){
+//     // console.log(req.params.id);
+//     // if (books.title != null){
+//     Rsv.findById(books.title, function(err, rsvs){
+//         console.log(rsvs);
+//         let up_book = {_id:books._id};
+//         let book={};
+
+
+//         if (rsvs == null || (req.user.id != rsvs.uid)){
+//           if (rsvs == null){
+//             console.log("rsvs is null");
+//           }
+//           else{
+//             console.log("new uid");
+//           }
+          
+//         //  // req.flash('success', 'test');
+//         //   new_rsv = new Rsv({
+//         //     title : books.title,
+//         //     professor : books.professor,
+//         //     uid : req.user.id,
+//         //     num_rsv : req.body.count
+//         //   });
+//         //   console.log(new_rsv);
+//         //   new_rsv.save();
+
+          
+//         //   book.stock = books.stock - new_rsv.num_rsv;
+//         //   book.total_rsv = books.total_rsv + new_rsv.num_rsv;
+          
+//         }
+
+
+
+//         else{
+//           let up_rsv = {_id:rsv._id};
+//           let rsv={};
+//           rsv.title = rsvs.title;
+//           rsv.professor = rsvs.professor;
+//           rsv.uid = rsvs.uid;
+//           rsv.num_rsv = req.body.count;
+
+//           Rsv.update(up_rsv, rsv, function(err){
+//             if(err){
+//               console.log(err);
+//               return;
+//             } else {
+//               req.flash('success', '수정 완료!!');
+//             }
+//           });
+
+//           book.stock = books.stock + rsvs.num_rsv - rsv.num_rsv;
+//           book.total_rsv = books.total_rsv - rsvs.num_rsv + rsv.num_rsv;
+//         }
+//         // if (rsv != null && req.user.id != rsv.uid){
+//           Book.update(up_book, book, function(err){
+//             if(err){
+//               console.log(err);
+//               return;
+//             } else {
+//               req.flash('success', '예약 완료!!');
+//             }
+//           });
+
+//           res.redirect('/booklookup');
+//         // }
+//       });   
+//     // } 
+//   });
+  
     
-    //num_rsv를 업데이트하는 작업
-    let book={};
-    book.num_rsv = books.num_rsv;
-    let up_book = {_id:books._id};
-    Book.update(up_book, book, function(err){
-      if(err){
-        console.log(err);
-        return;
-      } else {
-        req.flash('success', '예약 완료!!');
-      }
-    });
-  });
-  var books = await Book.find();
-  res.redirect('/booklookup');
-});
+
+
+
+
+//   //   if (books.uid == null || books.uid != req.params.id){
+//   //     // req.flash('success', 'test');
+//   //     if(books.num_rsv+Number(req.body.count)>books.stock){
+//   //       req.flash('fail', '잔여량보다 더 많은 양을 예약했어용');
+//   //       return res.render('booklookup/reserve', {books:books});
+//   //     }
+//   //     books.num_rsv = Number(req.body.count);
+//   //     books.total_rsv += books.num_rsv;
+//   //     console.log(books.num_rsv);
+//   //     console.log(books.total_rsv);
+      
+//   //     //num_rsv를 업데이트하는 작업
+//   //     book = new Book({
+//   //       title : books.title,
+//   //       professor : books.professor,
+//   //       price : books.price,
+//   //       stock : books.stock - books.total_rsv,
+//   //       uid : req.params.id,
+//   //       num_rsv : books.num_rsv,
+//   //       total_rsv : books.total_rsv        
+//   //     });
+
+//   //     book.save();
+//   //   }
+//   //   else {
+//   //     reserveBook(req.body, req.params.id, "update");
+//   //   }
+
+  
+    
+//   // var books = await Book.find();
+//   // res.redirect('/booklookup');
+// });
 
 
 module.exports = router;
